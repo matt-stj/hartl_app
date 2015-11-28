e99f0e59d4d4b9a9fb15626c68f19be72787bfab
Matt Stjernholm <thew88@gmail.com>
2015-11-28 17:37:14 -0600

    Add account ativations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,19 +28,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
   end
-
-  def destroy
-   User.find(params[:id]).destroy
-   flash[:success] = "User deleted"
-   redirect_to users_url
- end
 
   def edit
     @user = User.find(params[:id])

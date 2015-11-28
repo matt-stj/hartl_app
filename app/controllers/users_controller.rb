42080af24859107d53a520ef29a74dfc00e1794c
Matt Stjernholm <thew88@gmail.com>
2015-11-28 12:39:52 -0600

    log in and out complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -7,9 +9,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def create
-     @user = User.new(user_params)
+    @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -17,11 +30,22 @@ class UsersController < ApplicationController
     end
   end
 
-  private
-
-  def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+  def edit
+    @user = User.find(params[:id])
   end
 
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,15 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
